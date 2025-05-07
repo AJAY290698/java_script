1361ec7d6ce02ac22090d6fb45ad347640c5274c
AJAY290698 <ajaypatil703@gmail.com>
2025-05-07 13:58:16 +0000

    data_types

---
+++ basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict";// treat all js code as newner version
+
+//alert (3 + 3) // we are using nodejs, not browser
+
+console.log(1200*836);
+console.log("ajay")
+
+let name = "Ajay"
+let age = 18
+let isLoggedIn = false
+
+
+// Data Types
+
+// number => 2 to power 53
+// bigint
+//string => ""
+//boolean => true/false
+//null => standalone value
+// undefined
+// symbol =>unique
+
+//object 
+
+console.log(typeof null);//object
+
+console.log(typeof undefined);//undefined
+
---
+++ basics/variables.js
@@ -0,0 +1,11 @@
+const AccountId = 051194
+let accountemail = "ajay@google.com"
+var accountpassword = "12345"
+accountcity = "kolhapur"
+let accountstate
+
+accountemail = "swati@google.com"
+accountpassword ="290698"
+//AccountId =123
+
+console.table ([AccountId, accountemail, accountpassword,  accountcity, accountstate])
