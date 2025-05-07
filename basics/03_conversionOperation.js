44ea976a29e9ac44d81985e0aeaba9c3518e2fa0
AJAY290698 <ajaypatil703@gmail.com>
2025-05-07 18:24:41 +0000

    conversion of data types

---
+++ basics/03_conversionOperation.js
@@ -0,0 +1,27 @@
+let score = false
+
+//console.log (typeof score);
+//console.log (typeof (score));
+
+//let valueInNumber = Number(score)
+//console.log (typeof valueInNumber );
+//console.log (valueInNumber );
+
+//"33"=> 33
+//"33abc => NaN"
+// true =>1; false=>0
+
+let isLoggedIn = "ajay"
+
+let booleanIsLogedIn = Boolean (isLoggedIn)
+console.log (booleanIsLogedIn)
+
+//1 => true; 0=>false
+// ""=> fasle
+// "Ajay"=>true
+
+let somenumber =29
+
+let stringnumner = String(somenumber)
+console.log (stringnumner)
+console.log (typeof stringnumner)
